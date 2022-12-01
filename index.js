20a9899974c97d61422613ebda16270c1843d3c2
lakshmida <lakshmi.kasimsetti@gmail.com>
2022-12-01 10:17:57 +0000

    solution

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const peopleNames = require("../country/state/city/index");
 const getFirstNames = require("../utilities/utils/index");
+const name = "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";
 
 const getPeopleInCity = (peopleNames) => {
   return getFirstNames(peopleNames);
